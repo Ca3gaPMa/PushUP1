17a5b3ddec84b70cfcd7b23ea726c7a861415c92
Ca3gaPMa <christian_krasimirov@yahoo.com>
2017-12-31 06:29:34 +0100

    AddFile Test

---
+++ app/src/main/java/com/example/org/pushup/TestAddFile.java
@@ -0,0 +1,14 @@
+package com.example.org.pushup;
+
+/**
+ * Created by Ca3gaPMa on 31.12.2017.
+ */
+
+public class TestAddFile {
+
+    private int test;
+
+    public TestAddFile(int test){
+        this.test = test;
+    }
+}
